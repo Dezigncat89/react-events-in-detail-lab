ff6d7ab0b6e8dbe9ab80339c9d438865ecdaad3e
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-05-19 12:53:37 -0700

    Update CoordinatesButton.js

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,9 +1,40 @@
-import React from "react"
+import React, {Component} from "react"
 
 class CoordinatesButton extends React.Component
   constructor() {
-    this.state = {
-      xCoordinate = 0;
-      yCoordinate = 0; 
+    super()
+      this.state = {
+        xPosition: 0,
+        yPosition: 0
    }
+   this.onReceiveCoordinates = this.onReceiveCoordinates.bind(this)
 }
+
+onReceiveCoordinates () {
+   while (element) {
+    if (element.tagName == "BODY") {
+      let xScroll = element.scrollLeft || document.documentElement.scrollLeft;
+      let yScroll = element.scrollTop || document.documentElement.scrollTop;
+ 
+      xPosition += (element.offsetLeft - xScroll + element.clientLeft);
+      yPosition += (el.offsetTop - yScroll + element.clientTop);
+    } else {
+      xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+      yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+    }
+ 
+    element = element.offsetParent;
+    }
+}
+  
+  
+render () {
+  return {
+    <h1>{this.state.xPosition}>X Location:</h1>
+    <h2>{this.state.yPosition}>Y Location:</h2>
+  };
+}
+
+
+export default CoodinatesButton
+
