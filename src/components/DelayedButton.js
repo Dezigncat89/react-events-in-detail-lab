823d349813f6575f6c42499cf1cdfdfe313ae771
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-05-19 13:07:59 -0700

    Update DelayedButton.js

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,21 @@
-// Code DelayedButton Component Here
+import React, { useEffect, useState } from 'react';
+
+function onDelayedClick() {
+  
+  const [count, setCount] = useState(0);
+  const countRef = useRef(count);
+  countRef.current = count;
+
+  const getCountTimeout = () => {
+    setTimeout(() => {
+      setTimeoutCount(countRef.current);
+    }, 2000);
+  };
+  
+   return (
+     ...
+  )
+  
+}
+   
+export default onDelayedLClick;
